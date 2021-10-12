cd4afffbb799fa2d12dd2bef84d181d0a22895d5
ThatSameBlackBoyFromAfrica <muofunanya3@gmail.com>
2021-10-12 20:46:33 +0100

    completed the git get and follow api route

--- routes/users.js
+++ routes/users.js
@@ -27,7 +27,6 @@ router.put("/:id", async (req, res) => {
   }
 });
 
-
 //Delete User
 router.delete("/:id", async (req, res) => {
   if (req.body.userId === req.params.id || req.body.isAdmin) {
@@ -42,9 +41,39 @@ router.delete("/:id", async (req, res) => {
   }
 });
 
-
 //Get a User
+router.get("/:id", async (res, req) => {
+  try {
+    const user = await User.findById(req.params.id);
+    const {password, updatedAt, ...other} = user._doc;
+    res.status(200).json(user);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
 //Follow a User
+router.put('/:id/follow', async(res, req) => {
+  if(req.body.userId !== req.params.id){
+    try{
+      const user = await User.findById(req.params.id);
+      const currentUser = await user.findById(req.body.userId);
+
+      if(!user.followers.includes(req.body.userId)){
+         await user.updatedOne({$push: {followers: req.body.userId}})
+         await curentUser.updatedOne({$push: {followerings: req.params.id}})
+         res.status(200).json("user has been followed")
+      } else {
+        res.status(403).json("You already follow the user")
+      }
+    } catch {
+      res.status(500).json(err)
+    }
+  } else {
+    res.status(403).json("You cannot follow yourself")
+  }
+})
+
 //Unfollow a User
 
 router.get("/", (req, res) => {
